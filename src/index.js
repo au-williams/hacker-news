42b1d6d09d3582e9d061490abb175ba6f6a3c781
Austin Williams <me@austinwilliams.dev>
2021-08-20 17:48:22 -0700

    added hash router to index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
